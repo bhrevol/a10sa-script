b33911a88c6d1fcd5e3cba2d55d577be48479dfd
byeonhyeok <bhrevol@gmail.com>
2022-01-13 23:31:30 +0900

    script: add vcsx onarhythm support

--- tests/script/test_vcsx.py
+++ tests/script/test_vcsx.py
@@ -1,15 +1,36 @@
 """Test cases for VCSX scripts."""
 import io
+from typing import List
+from typing import Type
 
 import pytest
 
 from a10sa_script.command.vorze import VorzeRotateCommand
+from a10sa_script.command.vorze import VorzeVibrateCommand
 from a10sa_script.exceptions import ParseError
 from a10sa_script.script.vcsx import VCSXCycloneScript
+from a10sa_script.script.vcsx import VCSXOnaRhythmScript
+from a10sa_script.script.vcsx import VCSXScript
+from a10sa_script.script.vorze import _T
 from a10sa_script.script.vorze import VorzeScriptCommand
 
 
-TEST_CYCLONE = (
+ONARHYTHM_DATA = (
+    b"VCSX\x01Vorze_OnaRhythm\x00\x01\x42"
+    b"\x00\x00\x00\x03"  # 3 commands
+    b"\x00\x00\x00\x00\x00"  # 0, 0, True
+    b"\x00\x00\xB4\xC8\x14"  # 46280, 20
+    b"\x00\x00\xB5\x0A\x1A"  # 46346, 26
+)
+
+ONARHYTHM_COMMANDS = [
+    VorzeScriptCommand(0, VorzeVibrateCommand(0)),
+    VorzeScriptCommand(46280, VorzeVibrateCommand(20)),
+    VorzeScriptCommand(46346, VorzeVibrateCommand(26)),
+]
+
+
+CYCLONE_DATA = (
     b"VCSX\x01Vorze_CycloneSA\x00\x01\x42"
     b"\x00\x00\x00\x05"  # 5 commands
     b"\x00\x00\x00\x00\x00"  # 0, 0, True
@@ -19,8 +40,7 @@ TEST_CYCLONE = (
     b"\x00\x00\xB5\x0A\x1A"  # 46346, 26, True
 )
 
-
-TEST_COMMANDS = [
+CYCLONE_COMMANDS = [
     VorzeScriptCommand(0, VorzeRotateCommand(0, True)),
     VorzeScriptCommand(46079, VorzeRotateCommand(6, False)),
     VorzeScriptCommand(46179, VorzeRotateCommand(13, False)),
@@ -29,33 +49,59 @@ TEST_COMMANDS = [
 ]
 
 
-def test_cyclone_load() -> None:
-    """Test loading cyclone script from VCSX."""
-    orig = io.BytesIO(TEST_CYCLONE)
+@pytest.mark.parametrize(
+    "script_cls, data, commands",
+    [
+        (VCSXOnaRhythmScript, ONARHYTHM_DATA, ONARHYTHM_COMMANDS),
+        (VCSXCycloneScript, CYCLONE_DATA, CYCLONE_COMMANDS),
+    ],
+)
+def test_load(
+    script_cls: Type[VCSXScript[_T]],
+    data: bytes,
+    commands: List[VorzeScriptCommand[_T]],
+) -> None:
+    """Test loading script from VCSX."""
+    orig = io.BytesIO(data)
     orig.seek(0)
-    script = VCSXCycloneScript.load(orig)
-    expected = VCSXCycloneScript(TEST_COMMANDS)
+    script = script_cls.load(orig)
+    expected = script_cls(commands)
     assert script == expected
 
 
-def test_cyclone_dump() -> None:
-    """Test dumping cyclone script to VCSX."""
-    script = VCSXCycloneScript(TEST_COMMANDS)
+@pytest.mark.parametrize(
+    "script_cls, data, commands",
+    [
+        (VCSXOnaRhythmScript, ONARHYTHM_DATA, ONARHYTHM_COMMANDS),
+        (VCSXCycloneScript, CYCLONE_DATA, CYCLONE_COMMANDS),
+    ],
+)
+def test_cyclone_dump(
+    script_cls: Type[VCSXScript[_T]],
+    data: bytes,
+    commands: List[VorzeScriptCommand[_T]],
+) -> None:
+    """Test dumping script to VCSX."""
+    script = script_cls(commands)
     f = io.BytesIO()
     script.dump(f)
-    assert f.getvalue() == TEST_CYCLONE
+    assert f.getvalue() == data
 
 
-def test_rotate_load_invalid() -> None:
-    """Test invalid CSV parsing."""
+@pytest.mark.parametrize(
+    "script_cls, data",
+    [(VCSXOnaRhythmScript, ONARHYTHM_DATA), (VCSXCycloneScript, CYCLONE_DATA)],
+)
+def test_rotate_load_invalid(script_cls: Type[VCSXScript[_T]], data: bytes) -> None:
+    """Test invalid VCSX parsing."""
     # invalid header magic
-    orig = io.BytesIO(b"1234")
+    orig = io.BytesIO(b"\x00")
     with pytest.raises(ParseError):
         orig.seek(0)
-        VCSXCycloneScript.load(orig)
+        script_cls.load(orig)
 
     # invalid data
-    orig = io.BytesIO(TEST_CYCLONE[:-3])
+    orig = io.BytesIO(data[:-3])
     with pytest.raises(ParseError):
         orig.seek(0)
-        VCSXCycloneScript.load(orig)
+        script_cls.load(orig)
