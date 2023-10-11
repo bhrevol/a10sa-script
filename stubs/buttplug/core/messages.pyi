755ee1565c69ebaedeb6d5e2ef69515391f5a178
byeonhyeok <bhrevol@gmail.com>
2023-10-11 18:20:24 +0900

    pre-commit: update stubs

--- stubs/buttplug/core/messages.pyi
+++ stubs/buttplug/core/messages.pyi
@@ -2,7 +2,6 @@ from typing import Any
 from typing import Dict
 from typing import List
 
-
 class ButtplugMessage:
     SYSTEM_ID: int
     DEFAULT_ID: int
--- stubs/sortedcontainers.pyi
+++ stubs/sortedcontainers.pyi
@@ -7,7 +7,6 @@ from typing import TypeVar
 from typing import Union
 from typing import overload
 
-
 T = TypeVar("T")
 
 class SortedList(MutableSequence[T]):
