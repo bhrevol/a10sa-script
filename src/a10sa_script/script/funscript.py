53a62b0181866f33f9ba7b15ed895de28c0c824e
byeonhyeok <bhrevol@gmail.com>
2023-11-02 19:10:15 +0900

    funscript: drop unnecessary end position actions

--- src/a10sa_script/script/funscript.py
+++ src/a10sa_script/script/funscript.py
@@ -1,16 +1,23 @@
 """Funjack funscript script module."""
 import io
 import json
+from dataclasses import asdict
+from dataclasses import dataclass
 from typing import BinaryIO
-from typing import Dict
+from typing import Iterator
 from typing import List
-from typing import Optional
 
 from ..command import VorzeLinearCommand
 from ..exceptions import ParseError
 from .vorze import _SC
 from .vorze import VorzeLinearScript
 from .vorze import VorzeScriptCommand
+
+
+@dataclass(frozen=True)
+class _Action:
+    at: int
+    pos: int
 
 
 class FunscriptScript(VorzeLinearScript):
@@ -30,6 +37,7 @@ class FunscriptScript(VorzeLinearScript):
 
     FUNSCRIPT_VERSION = "1.0"
     OFFSET_DENOM = 1
+    CONVERSION_THRESHOLD_MS = 100
 
     def dump(self, fp: BinaryIO) -> None:
         """Serialize script to file.
@@ -38,43 +46,50 @@ class FunscriptScript(VorzeLinearScript):
             fp: A file-like object opened for writing.
         """
         with io.TextIOWrapper(fp, newline="") as text_fp:
-            actions: List[Dict[str, int]] = []
-            pos = 1.0
-            for cmd in self.commands:
-                # Generate two funscript position updates for every movement
-                # (one for start position and one for end position).
-                actions.append({"at": cmd.offset, "pos": self.pos_from_vector(pos)})
-                duration, newpos = cmd.cmd.vectors(pos)[0]
-                actions.append(
-                    {"at": cmd.offset + duration, "pos": self.pos_from_vector(newpos)}
-                )
-                pos = newpos
-            self._fixup_actions(actions)
             data = {
                 "version": self.FUNSCRIPT_VERSION,
                 "inverted": False,
                 "range": 90,
-                "actions": actions,
+                "actions": [asdict(action) for action in self.actions()],
             }
             json.dump(data, text_fp)
 
-    @staticmethod
-    def _fixup_actions(actions: List[Dict[str, int]]) -> None:
-        max_offset: Optional[int] = None
-        for i in range(len(actions) - 1, -1, -1):
-            action = actions[i]
-            if max_offset is None:
-                max_offset = action["at"]
-                continue
-            if action["at"] >= max_offset:
-                # For actions which are close together (i.e. high speed piston
-                # movements) speed conversion can result in durations that are
-                # computed to end past the start of the next movement. When
-                # this happens we just drop the end position update for the
-                # offending movement.
-                del actions[i]
-            else:
-                max_offset = action["at"]
+    def actions(self) -> Iterator[_Action]:
+        """Iterate over this script's commands as Funscript actions.
+
+        Funscript movements consist of two actions - start point and end
+        point. For movements which are close together in time offset, (i.e.
+        high speed piston movements) speed->duration conversion can cause:
+
+            - durations that are computed to end past the start of the next
+              movement
+            - end points that are too close in time to the next start point
+
+        In both of these cases, we can drop the offending end point for the
+        preceding action without affecting the actual movement pattern.
+
+        The threshold used for determining whether to drop end points can be
+        set via `FunscriptScript.CONVERSION_THRESHOLD_MS` (defaults to 100 ms).
+        Note that this is only the threshold for dropping movement end points.
+        There is no minimum restriction on the time offset difference between
+        consecutive movement start points.
+
+        Yields:
+            Funscript actions in order.
+        """
+        pos = 1.0
+        for i, cmd in enumerate(self.commands):
+            start = _Action(cmd.offset, self.pos_from_vector(pos))
+            duration, endpos = cmd.cmd.vectors(pos)[0]
+            end = _Action(cmd.offset + duration, self.pos_from_vector(endpos))
+            yield start
+            try:
+                next_cmd = self.commands[i + 1]
+                if next_cmd.offset - end.at > self.CONVERSION_THRESHOLD_MS:
+                    yield end
+            except IndexError:
+                yield end
+            pos = endpos
 
     @classmethod
     def load(cls, fp: BinaryIO) -> "FunscriptScript":
