499035b3e7468273fde484377984958c3e5154d1
byeonhyeok <bhrevol@gmail.com>
2026-03-03 23:30:07 +0900

    test against py314

--- noxfile.py
+++ noxfile.py
@@ -11,7 +11,7 @@ from nox import Session, session
 os.environ.update({"PDM_IGNORE_SAVED_PYTHON": "1"})
 
 package = "a10sa_script"
-python_versions = ["3.13", "3.12", "3.11"]
+python_versions = ["3.14", "3.13", "3.12", "3.11"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
